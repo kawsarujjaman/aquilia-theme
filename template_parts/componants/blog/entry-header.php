f70a52ff58e11d9cd7850764c57c27e5b91ee099
Kawsarujjaman <kaws01717@gmail.com>
2022-11-22 21:16:51 +0600

    return back

---
+++ template_parts/componants/blog/entry-header.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *The Template for post entry header
+ * @package Aquilia
+ */
+
+ $the_post_id = get_the_ID();
+ $hide_title = get_post_meta( $the_post_id, '_hide_post_id', true);
+ echo '<pre>';
+ print_r($the_post_id. ' ' . $hide_title);
+ wp_die();
+$has_post_thumbnail = get_the_post_thumbnail();
+ ?>
+
+
+ <header class="entry-header">
+    <?php
+    // Feateur Image
+
+    if(has_post_thumbnail()){
+        ?>
+        <div class="entry-image mb-3">
+            <a href="<?php echo esc_url(get_permalink());?>">
+
+            <?php ;
+            the_post_custom_thumbnail(
+                $the_post_id,
+                'featured-thumbnail',
+                [
+                    'sizes' => '(max-width: 590px), 350px, 235px',
+                    'class' => 'attachement-feature-large size-featured-image'
+                ]
+            );
+            ?>
+        </a>
+        </div>
+        <?php
+    }
+    ?>
+ </header>
---
+++ template_parts/content.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Content Template
+ * @package Aquilia
+ */
+
+ ?>
+ 
+ <article id="post-<?php the_ID();?>" <?php post_class('mb-5'); ?>>
+<?php 
+get_template_part('template_parts/componants/blog/entry-content');
+get_template_part('template_parts/componants/blog/entry-header');
+get_template_part('template_parts/componants/blog/entry-meta');
+get_template_part('template_parts/componants/blog/entry-footer');
+
+?>
+</article>
+
