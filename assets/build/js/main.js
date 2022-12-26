27f110680963a1ee11ab1b468a9796f4fd354056
Kawsarujjaman <kaws01717@gmail.com>
2022-12-27 02:18:49 +0600

    search box add in header section

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -27,7 +27,26 @@ jQuery(document).ready(function () {
     // autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
   });
 });
 
