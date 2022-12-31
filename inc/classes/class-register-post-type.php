ac12f4910e28cb988f6003c13db954d320dbc0cd
Kawsarujjaman <kaws01717@gmail.com>
2023-01-01 04:36:02 +0600

    Cusotom post type

---
+++ inc/classes/class-register-post-type.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Register PostType
+ * 
+ * @package Aquilia
+ */
+
+ namespace AQUILIA_THEME\Inc;
+
+ use AQUILIA_THEME\Inc\Traits\Singleton;
+
+ class register_post_types {
+    use Singleton;
+
+    protected function __construct()
+    {
+        /**
+         * Actions.
+         */
+    }
+
+    // Register Custom post type movie
+    
+ }
