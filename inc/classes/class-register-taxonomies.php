4abf9c1002642a96b140a7d18e66a9a08e8670fa
Kawsarujjaman <kaws01717@gmail.com>
2023-01-01 05:35:35 +0600

    Update class-register-taxonomies.php

--- inc/classes/class-register-taxonomies.php
+++ inc/classes/class-register-taxonomies.php
@@ -22,6 +22,7 @@
          * Actions
          */
         add_action( 'init', [$this, 'create_genre_taxonomy'] );
+        add_action( 'init', [$this, 'create_year_taxonomy'] );
     }
 
     public function create_genre_taxonomy() {
@@ -54,4 +55,39 @@
         ];
         register_taxonomy( 'genre', ['movies'], $args);
     }
+
+    
+    // Register Taxonomy Year
+	public function create_year_taxonomy() {
+
+		$labels = [
+			'name'              => _x( 'Years', 'taxonomy general name', 'aquilia' ),
+			'singular_name'     => _x( 'Year', 'taxonomy singular name', 'aquilia' ),
+			'search_items'      => __( 'Search Years', 'aquilia' ),
+			'all_items'         => __( 'All Years', 'aquilia' ),
+			'parent_item'       => __( 'Parent Year', 'aquilia' ),
+			'parent_item_colon' => __( 'Parent Year:', 'aquilia' ),
+			'edit_item'         => __( 'Edit Year', 'aquilia' ),
+			'update_item'       => __( 'Update Year', 'aquilia' ),
+			'add_new_item'      => __( 'Add New Year', 'aquilia' ),
+			'new_item_name'     => __( 'New Year Name', 'aquilia' ),
+			'menu_name'         => __( 'Year', 'aquilia' ),
+		];
+		$args   = [
+			'labels'             => $labels,
+			'description'        => __( 'Movie Release Year', 'aquilia' ),
+			'hierarchical'       => true,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'show_in_nav_menus'  => true,
+			'show_tagcloud'      => true,
+			'show_in_quick_edit' => true,
+			'show_admin_column'  => true,
+			'show_in_rest'       => true,
+		];
+		register_taxonomy( 'movie-year', [ 'movies' ], $args );
+    }
+
  }
