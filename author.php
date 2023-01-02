9d23ec652724e207de7418029b935995717d85c4
Kawsarujjaman <kaws01717@gmail.com>
2023-01-02 11:04:46 +0600

    author page created

---
+++ author.php
@@ -0,0 +1,27 @@
+
+<?php
+/**
+ * Author page template file
+ * 
+ * @package Aquilia
+ */
+if( !defined('ABSPATH')) exit;
+
+get_header();
+
+$author = get_queried_object();
+
+?>
+
+
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <div class="container">        
+          
+        </div>
+             
+    </main>
+</div>
+
+
+<?php get_footer();?>
